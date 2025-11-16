25df240f8edda0c229476a5f6e87a176b96821ad
Nitish Kumar Mandal <147975324+Nitish-kr-Mandal@users.noreply.github.com>
2025-11-16 12:59:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function firstChar(text) {
 	return trimText.charAt(0); 
 }
 
-Do not change the code below
-Uncomment the following line to show the prompt popup
+// Do not change the code below
+// Uncomment the following line to show the prompt popup
 const text = prompt("Enter text:"); 
 alert(firstChar(text));
